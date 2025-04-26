938943d2177dd05611d3252889b6bbc706d77a80
RemiVibert <remi.vibert.efrei@gmail.com>
2025-04-26 17:09:23 +0200

    debug : affichage des hitbox des collectibles

--- collectible.py
+++ collectible.py
@@ -29,4 +29,13 @@ class Collectible:
         scaled_image = pygame.transform.rotozoom(self.original_image, 0, camera.zoom) # Applique un zoom à l'image de la planète
         scaled_image = pygame.transform.rotate(scaled_image, self.rotation)
         new_rect = scaled_image.get_rect(center=camera.world_pos_to_screen_pos(self.pos)) # Calcule le rectangle de dessin pour que la planète soit centrée correctement
+        
+
+        
         screen.blit(scaled_image, new_rect) # Affiche l’image de la planète à l’écran
+
+        # === DEBUG == #
+        # Affiche le rect du collectible avec un rectangle vert transparent
+        rect_surface = pygame.Surface((new_rect.width, new_rect.height), pygame.SRCALPHA)
+        rect_surface.fill((0, 255, 0, 50))  # Vert transparent # mettre le "50" à 0 pour rendre invisible
+        screen.blit(rect_surface, new_rect.topleft)
