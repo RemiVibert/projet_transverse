ff392fa10ef604df1ae49bc24dd505646118bee8
MyriamMou <myriam.mouttalib@efrei.net>
2025-05-09 20:37:11 +0200

    recadrage du rect de la base

--- base.py
+++ base.py
@@ -4,23 +4,23 @@ class Base:
     def __init__(self, x, y, image):
         self.image = image
         self.pos = pygame.Vector2(x, y)
-        original_rect = self.image.get_rect(center=(x, y))
-        reduction = 260
-
-        self.rect = pygame.Rect(
-            original_rect.x + reduction // 2,
-            original_rect.y + reduction // 2,
-            original_rect.width - reduction,
-            original_rect.height - reduction
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.collision_rect = pygame.Rect(
+            self.rect.x + 300,
+            self.rect.y + 300,
+            self.rect.width - 600,
+            self.rect.height - 600
         )
-
 
     def draw(self, screen, camera):
         transformed_rect = camera.apply(self)
         screen.blit(pygame.transform.scale(self.image, transformed_rect.size), transformed_rect.topleft)
 
     def check_collision(self, player_rect):
-        return self.rect.colliderect(player_rect)
+        return self.collision_rect.colliderect(player_rect)
 
     def get_rect(self):
         return self.rect
+
+    def get_collision_rect(self):
+        return self.collision_rect
