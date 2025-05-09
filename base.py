a9dfb72b7dc3fbb66a65d4b3ed76c505989d5465
MyriamMou <myriam.mouttalib@efrei.net>
2025-05-09 20:24:21 +0200

    début de fix du rect de la base trop grand

--- base.py
+++ base.py
@@ -4,18 +4,23 @@ class Base:
     def __init__(self, x, y, image):
         self.image = image
         self.pos = pygame.Vector2(x, y)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
+        original_rect = self.image.get_rect(center=(x, y))
+        reduction = 100
+
+        self.rect = pygame.Rect(
+            original_rect.x + reduction // 2,
+            original_rect.y + reduction // 2,
+            original_rect.width - reduction,
+            original_rect.height - reduction
+        )
+
 
     def draw(self, screen, camera):
         transformed_rect = camera.apply(self)
         screen.blit(pygame.transform.scale(self.image, transformed_rect.size), transformed_rect.topleft)
 
     def check_collision(self, player_rect):
-        base_rect = pygame.Rect(self.pos.x, self.pos.y, self.image.get_width(), self.image.get_height())
-        return base_rect.colliderect(player_rect)
+        return self.rect.colliderect(player_rect)
 
     def get_rect(self):
-        width = self.image.get_width()
-        height = self.image.get_height()
-        return pygame.Rect(self.pos.x, self.pos.y, width, height)
+        return self.rect
