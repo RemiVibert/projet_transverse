9ad1bab2f186f48680e1685da09015cbb9946053
MyriamMou <myriam.mouttalib@efrei.net>
2025-05-10 19:47:04 +0200

    commentaires de base.py

--- base.py
+++ base.py
@@ -4,8 +4,8 @@ class Base:
     def __init__(self, x, y, image):
         self.image = image
         self.pos = pygame.Vector2(x, y)
-        self.rect = self.image.get_rect(topleft=(x, y))
-        self.collision_rect = pygame.Rect(
+        self.rect = self.image.get_rect(topleft=(x, y)) # Rectangle de l'image pour gestion de la position
+        self.collision_rect = pygame.Rect( # Rectangle de collision avec un offset et des dimensions réduites
             self.rect.x + 300,
             self.rect.y + 300,
             self.rect.width - 600,
@@ -13,14 +13,18 @@ class Base:
         )
 
     def draw(self, screen, camera):
+        # Applique la transformation de la caméra à la base pour l'afficher correctement
         transformed_rect = camera.apply(self)
         screen.blit(pygame.transform.scale(self.image, transformed_rect.size), transformed_rect.topleft)
 
     def check_collision(self, player_rect):
+        # Vérifie si le rectangle de collision de la base entre en contact avec celui du joueur
         return self.collision_rect.colliderect(player_rect)
 
     def get_rect(self):
+        # Retourne le rectangle de l'image
         return self.rect
 
     def get_collision_rect(self):
+        # Retourne le rectangle de collision
         return self.collision_rect
